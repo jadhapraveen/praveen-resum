2092974252c4b04b5c5352568ef43e040581d23e
jadhavpraeen <jadhavpravinpns@gmail.com>
2023-12-16 11:18:27 +0530

    code push to github

---
+++ test.js
@@ -0,0 +1,116 @@
+// console.log ("Hello world")
+// let x
+// let y
+// let z
+// x=6
+// y=4
+// z=x+y
+// console.log(z)
+
+
+// let x,y,z
+
+// x=6
+// y=4
+// z=x+y
+// console.log(z)/ 
+
+// data type :number
+
+// let x=6
+// let y=4
+// let z=x+y
+// console.log(z)
+// console.log(typeof x)
+
+// student name
+// let name="praveen "
+// console.log(typeof name)
+
+
+// let num1 =2
+// let output = num1 ** 3 //2*2*2
+// console.log(output)
+
+
+// let num1 =9
+// let output = num1 ** 3 //2*2*2
+// console.log(output)
+
+// console.log(num1%2)
+
+
+// let num1 =14
+// let output = num1 ** 3 //2*2*2
+// console.log(output)
+
+// console.log(num1%5)
+
+
+
+// let x=5
+// x=x+3
+
+// console.log("x value:",x)
+
+
+// let y=5
+// y=y -2
+
+// console.log("y value:",y)
+
+// shortHand above code
+// let x =5
+// x+=3
+
+// console.log("x value:",x)
+
+// let y =5
+// y -=3
+
+// console.log("y value:",y)
+
+
+// let z=9
+// z=z%2
+// console.log("z")
+
+
+// let firstName="praveen"
+// let lastName ="jadhav"
+// let fullName =firstName+lastName
+// console.log ("fullName",fullName)
+
+// let firstName="praveen"
+// let lastName ="jadhav"
+// let fullName =firstName+","+lastName
+// console.log ("fullName",fullName)
+
+// let firstName="praveen"
+// let lastName ="jadhav"
+// let fullName =firstName+","+"lastName"
+// console.log ("fullName",fullName)
+
+// let x=5
+// let y=19
+//  console.log(5+19)
+
+// declare var x ;let x ;const x
+//            var       let       const
+// redclare
+
+// reassing
+
+
+// var x=5
+// console.log("first",x)
+
+// var x=9
+// console,log("second",x)
+
+// let x =5
+// console.log("first",x)
+
+
+
+
