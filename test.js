c25d3da7467625893cf4d1c8749d26c447b9b74b
jadhavpraeen <jadhavpravinpns@gmail.com>
2023-12-27 22:43:43 +0530

    fill added

--- test.js
+++ test.js
@@ -103,10 +103,10 @@
 // let fullName =firstName+","+lastName
 // console.log ("fullName",fullName)
 
-let firstName="praveen"
-let lastName ="jadhav"
-let fullName =firstName+" "+"lastName"
-console.log ("fullName",fullName)
+// let firstName="praveen"
+// let lastName ="jadhav"
+// let fullName =firstName+" "+"lastName"
+// console.log ("fullName",fullName)
 
 // let x="praveen"
 // let y="jadhav"
@@ -140,28 +140,123 @@ console.log ("fullName",fullName)
 
 // student name
 // let name="praveen "
-// console.log(typeof name
-// declare var x ;let x ;const x
-//            var       let       const
-// redclare
+// console.log(typeof name)
 
-// reassing
+// using var
+// using let
+// using const
 
+//declare  var x ;let x ;const x
+//assing x=5
+
+//difference
+ //           var       let       const
+
+// redclare   yes       no         no
+// reassing   yes       yes        no
 
 // var x=5
 // console.log("first",x)
 
-// var x=9
-// console,log("second",x)
+//   x=9
+//  console.log("second",x)
 
 // let x =5
 // console.log("first",x)
 
+//  x ="pravee"
+// console.log("second",x)
+
+// const x =5
+// console.log("first",x)
+
+//  x =9
+// console.log("second",x)
+
+// let x=5
+// let y=7
+
+// console.log("x before",x)
+
+//  x=y
+//  console.log("after",x)
+
+// let x=5
+// let y=7
+
+// console.log(x==y)
+
+// let x=5
+// let y=5
+
+// console.log(x==y)
+
+// let x=5
+// let y=6
+
+// console.log(x!=y)
+
+
+// let x=5
+// let y="5"
+
+// console.log("first",x==y)
+
+// let p=5
+// let q="5"
+
+// console.log("second",p===q)
+
+// let x=5
+// let y="5"
+
+// console.log("first",x==y)  //only value not data type
+
+// let p=5
+// let q="5"
+
+// console.log("second",p===q) // both volue and data type
+
+// let x=5
+// let y="5"
+
+// console.log("first",x==y)
+
+// let p=5
+// let q="5"
+
+// console.log("second",p!==q)
+
+//  x=y
+//  console.log("after",x)
+
+// console.log(8>9)
+
+// console.log(8<9)
+
+// console.log(8>8)
+
+// console.log(8<8)
+
+// console.log(8>=12)
+// console.log(8<=12)
+
+
+// let z=x
+// x=y
 // let z=9
 // z=z%2
 // console.log("z",z)
 
 
-let z=9
-z%2
-console.log("z",z)
+// let z=9
+// z%2
+// console.log("z",z)
+
+
+// let num1="5"
+// let num2=7
+// let num3=9
+// console.log(num3+num2+num1)
+
+
